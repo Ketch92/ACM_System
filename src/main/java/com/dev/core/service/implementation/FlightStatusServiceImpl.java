5e7965431aa2b7d684ec16ffebe08c801930fd0f
Oleh Kravets <oleg.kravets92@gmail.com>
2021-02-21 17:56:59 +0200

    removed postConstruct methods

--- src/main/java/com/dev/core/service/implementation/FlightStatusServiceImpl.java
+++ src/main/java/com/dev/core/service/implementation/FlightStatusServiceImpl.java
@@ -4,27 +4,14 @@ import com.dev.core.entity.FlightStatus;
 import com.dev.core.repository.FlightStatusRepository;
 import com.dev.core.service.FlightStatusService;
 import java.util.List;
-import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FlightStatusServiceImpl implements FlightStatusService {
-    private static final String ACTIVE_STATUS = "ACTIVE";
-    private static final String PENDING_STATUS = "PENDING";
-    private static final String DELAYED_STATUS = "DELAYED";
-    private static final String COMPLETED_STATUS = "COMPLETED";
     private final FlightStatusRepository flightStatusRepository;
     
     public FlightStatusServiceImpl(FlightStatusRepository flightStatusRepository) {
         this.flightStatusRepository = flightStatusRepository;
-    }
-    
-    @PostConstruct
-    private void injectBasicStatuses() {
-        flightStatusRepository.save(new FlightStatus(PENDING_STATUS));
-        flightStatusRepository.save(new FlightStatus(ACTIVE_STATUS));
-        flightStatusRepository.save(new FlightStatus(DELAYED_STATUS));
-        flightStatusRepository.save(new FlightStatus(COMPLETED_STATUS));
     }
     
     @Override
